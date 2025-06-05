200db91620889c8583424ff443b22c729cb92d46
anshulsc <anshulsinghchambial@gmail.com>
2025-06-05 20:06:51 +0530

    lora-qlora-finetuning

--- config.py
+++ config.py
@@ -16,3 +16,19 @@ class Configuration:
     batch_size: int = 8
     learning_rate: float = 2e-05
     epochs = 2
+    
+    # training type : "qlora", "lora", "full"
+    training_type: str = "full"
+    
+    lora_r: int = 16 
+    lora_alpha: int = 32
+    lora_dropout: float = 0.1
+    lora_target_modules: list = field(default_factory=lambda: ["q_proj", "k_proj", "v_proj", "o_proj"])
+    
+    bnb_4bit_use_double_quant: bool = True
+    bnb_4bit_quant_type: str = "nf4"
+    
+    project_name: str = "gemma3-object-detection"
+    run_name: str = "full"
+    
+    
--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import logging
 import wandb
 from functools import partial
+from dataclasses import asdict
 
 import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration, BitsAndBytesConfig
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
 from config import Configuration
 from utils import train_collate_function
@@ -25,7 +27,7 @@ augmentations = A.Compose([
 ], bbox_params=A.BboxParams(format='coco', label_fields=['category_ids'], filter_invalid_bboxes=True))
 
 
-def get_dataloader(processor):
+def get_dataloader(processor, cfg):
     logger.info("Fetching the dataset")
     train_dataset = load_dataset(cfg.dataset_id, split="train")
     train_collate_fn = partial(
@@ -62,21 +64,59 @@ def train_model(model, optimizer, cfg, train_dataloader):
 
 if __name__ == "__main__":
     cfg = Configuration()
+   
+    
+    
     processor = AutoProcessor.from_pretrained(cfg.model_id)
-    train_dataloader = get_dataloader(processor)
-
-    logger.info("Getting model & turning only attention parameters to trainable")
+    train_dataloader = get_dataloader(processor, cfg)
+    
+    
+    model_kwargs = {
+        "torch_dtype": cfg.dtype,
+        "device_map": cfg.device,
+        "attn_implementation": "eager",
+    }
+    
+    quantization_config = None
+    if cfg.training_type == "qlora":
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=cfg.bnb_4bit_use_double_quant,
+            bnb_4bit_quant_type=cfg.bnb_4bit_quant_type,
+        )
+        model_kwargs["quantization_config"] = quantization_config
+    else:
+        model_kwargs["quantization_config"] = None
+        
+    
     model = Gemma3ForConditionalGeneration.from_pretrained(
         cfg.model_id,
-        torch_dtype=cfg.dtype,
-        device_map="cpu",
-        attn_implementation="eager",
+        **model_kwargs,
     )
-    for name, param in model.named_parameters():
-        if "attn" in name:
-            param.requires_grad = True
-        else:
-            param.requires_grad = False
+    
+    if cfg.training_type == "full":
+        logger.info("Getting model & turning only attention parameters to trainable for full fine-tuning")
+        for name, param in model.named_parameters():
+            if "attn" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+                
+    elif cfg.training_type in ["qlora", "lora"]:
+        logger.info(f"Getting model & turning only attention parameters to trainable for {cfg.training_type.upper()}")
+        peft_config = LoraConfig(
+            r=cfg.lora_r,
+            lora_alpha=cfg.lora_alpha,
+            lora_dropout=cfg.lora_dropout,
+            target_modules=cfg.lora_target_modules,
+        )
+        
+        if cfg.training_type == "qlora":
+            model = prepare_model_for_kbit_training(model, usq_gradient_checkpointing=True)
+        
+        model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+        
 
     model.train()
     model.to(cfg.device)
@@ -84,6 +124,9 @@ if __name__ == "__main__":
     # Credits to Sayak Paul for this beautiful expression
     params_to_train = list(filter(lambda x: x.requires_grad, model.parameters()))
     optimizer = torch.optim.AdamW(params_to_train, lr=cfg.learning_rate)
+    
+    logger.info(f"Number of trainable parameters: {sum(p.numel() for p in params_to_train)}")
+    
 
     wandb.init(
         project=cfg.project_name,
